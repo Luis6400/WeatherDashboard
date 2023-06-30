0ff4304e21e3c4eb4d4e23e45fbe9e750eee49d7
luisvillarreal <secretangel21@outlook.com>
2023-06-30 00:18:53 -0600

    added code to actually set the cards and fixed recents

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -13,17 +13,41 @@ var cityname;
 var statename;
 var weatherurl;
 
+cityinput.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        search();
+    }
+});
+
+if (localStorage.getItem("city") !== null) {
+    renderrecents();
+};
+
+
 var apikey = "15ee655e9869842fef1d35bef76c5261";
 
+gobutton.addEventListener("click", search);
 
-$(gobutton).click(function () {
+
+function search() {
+
+    if (localStorage.getItem("city") === null) {
+        localStorage.setItem("city", JSON.stringify([]));
+    }
+
+    var savecity = cityinput.value;
 
 
     $("#gobutton").addClass("is-loading");
 
     city = cityinput.value.replace(/ /g, "%20");
     cityinput.value = "";
-    weatherurl = "https://api.openweathermap.org/data/2.5/forecast?q="+ city + "&units=imperial&appid=" + apikey +"";
+
+
+
+
+
+    weatherurl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apikey + "";
 
     if (weatherurl != null) {
         fetch(weatherurl)
@@ -32,69 +56,81 @@ $(gobutton).click(function () {
             })
             .then(function (data) {
                 console.log(data);
-                // get data here
-                // $("#citytitle").text(cityname + ", " + statename);
                 $("#gobutton").removeClass("is-loading");
-                // var weatherlist = data.list;
-                // createWeatherCards(weatherlist);
+
+                
+                    var arr =JSON.parse(localStorage.getItem("city"));
+                    arr.push(savecity);
+                    localStorage.setItem("city", JSON.stringify(arr));
+                    renderrecents();
+                
+
+                createWeatherCards(data.list);
             })
             .catch(function (error) {
                 $("#gobutton").removeClass("is-loading");
                 alert("City not found");
+                console.log(error);
             })
     }
 
+};
+
+function renderrecents() {
+
+    var recentstab = document.getElementById("recents");
+    recentstab.innerHTML = "";
+    var recents = JSON.parse(localStorage.getItem("city"));
+    k = recents.length-1;
+    for (var i = 0; i < 5; i++) {
+        if (recents[k] === undefined) {
+            break;
+        }
+        var recel = document.createElement("p");
+        recel.innerHTML = recents[k];
+        recel.addEventListener("click", function () {
+            cityinput.value = this.innerHTML;
+            search();
+        });
+        recentstab.appendChild(recel);
+        k--;
+    }
 
 
-    // state = stateinput.value;
+};
 
-    // stateinput.value = "AL";
-
-    // var cityurl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + ",US&limit=2&appid=15ee655e9869842fef1d35bef76c5261";
-
-    // fetch(cityurl)
-    //     .then(function (response) {
-    //         return response.json();
-    //     })
-    //     .then(function (data) {
-    //         console.log(data);
-    //         lat = encodeURIComponent(data[0].lat);
-    //         lon = encodeURIComponent(data[0].lon);
-    //         cityname = data[0].name;
-    //         statename = data[0].state;
-    //         // 40.6967254, lon: -111.986821
-    //         weatherurl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=15ee655e9869842fef1d35bef76c5261";
-    //         console.log(weatherurl);
-    //         getWeather(weatherurl);
-    //     })
-    //     .catch(function (error) {
-    //         $("#gobutton").removeClass("is-loading");
-    //         alert("City not found");
-    //     })
-
-
-
-    // });
-
-    // getWeather(weatherurl);
-    // console.log(weatherurl);
-
-
-});
-
-
-function getWeather(wurl) {
-    console.log(wurl);
-
-}
 
 function createWeatherCards(list) {
-    // create cards here
-    var currentdate = dayjs().format("YYYY-MM-DD");
-    // while (i<list.length) {
-    //     if()
-    // }
+    var k = 0;
+    for (var i = 0; i < 5; i++) {
 
+        var hi = list[k].main.temp_max;
+        var lo = list[k].main.temp_min;
+        var condition = list[k].weather[0].main;
+        var date = list[k].dt_txt;
+
+        var datehtmls = document.getElementsByClassName("date");
+        var hihtmls = document.getElementsByClassName("hi");
+        var lohtmls = document.getElementsByClassName("lo");
+        var conditionhtmls = document.getElementsByClassName("condition");
+        var imghtmls = document.getElementsByClassName("img");
+
+        datehtmls[i].innerHTML = date;
+        hihtmls[i].innerHTML = "high: " + hi;
+        lohtmls[i].innerHTML = "low: " + lo;
+        conditionhtmls[i].innerHTML = condition;
+        imghtmls[i].src = "https://openweathermap.org/img/w/" + list[k].weather[0].icon + ".png";
+
+
+
+
+
+
+
+        k = k + 8;
+
+
+    }
 
 
 }
